9acd60a91be3877f0bce221d11d372ea2a27f0b4
Marko <31570481+M4rkoHR@users.noreply.github.com>
2021-07-04 16:50:41 +0200

    Add support for arm and arm64

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@ GOOS=windows GOACH=amd64 go build -o deluge2qbt_v${1}_amd64.exe
 GOOS=windows GOARCH=386 go build -o deluge2qbt_v${1}_i386.exe
 GOOS=linux GOARCH=amd64 go build -o deluge2qbt_v${1}_amd64_linux
 GOOS=linux GOARCH=386 go build -o deluge2qbt_v${1}_i386_linux
+GOOS=linux GOARCH=arm go build -o deluge2qbt_v${1}_arm_linux
+GOOS=linux GOARCH=arm64 go build -o deluge2qbt_v${1}_arm64_linux
 GOOS=darwin GOARCH=amd64 go build -o deluge2qbt_v${1}_amd64_macos
 GOOS=darwin GOARCH=386 go build -o deluge2qbt_v${1}_i386_macos
